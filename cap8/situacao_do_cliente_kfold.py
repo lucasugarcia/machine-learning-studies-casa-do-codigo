bcbd042348db9d78f7b39f7c375e164cd335a3ae
Lucas Garcia <lucasugarcia@gmail.com>
2022-10-24 20:40:22 -0300

    Finalização do código do capítulo 8

--- cap8/situacao_do_cliente_kfold.py
+++ cap8/situacao_do_cliente_kfold.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from collections import Counter
+import numpy as np
+from sklearn.model_selection import cross_val_score
 
 df = pd.read_csv('situacao_do_cliente.csv')
 X_df = df[['recencia','frequencia', 'semanas_de_inscricao']]
@@ -21,18 +24,65 @@ treino_marcacoes = Y[:tamanho_de_treino]
 validacao_dados = X[tamanho_de_treino:]
 validacao_marcacoes = Y[tamanho_de_treino:]
 
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes):
+    k = 10
+    scores = cross_val_score(modelo, treino_dados, treino_marcacoes, cv = k)
+    taxa_de_acerto = np.mean(scores)
+
+    mensagem = "Taxa de acerto do {0}: {1}".format(nome, taxa_de_acerto)
+    print(mensagem)
+
+    return taxa_de_acerto
+
+resultados = {}
+
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
-from sklearn.model_selection import cross_val_score
-modelo = OneVsRestClassifier(LinearSVC(random_state=0))
+modelo_one_vs_rest = OneVsRestClassifier(LinearSVC(random_state = 0, max_iter = 100000))
+resultado_one_vs_rest = fit_and_predict("OneVsRest", modelo_one_vs_rest, treino_dados, treino_marcacoes)
+resultados[resultado_one_vs_rest] = modelo_one_vs_rest
 
-k = 10
+from sklearn.multiclass import OneVsOneClassifier
+modeloOneVsOne = OneVsOneClassifier(LinearSVC(random_state = 0, max_iter = 100000))
+resultadoOneVsOne = fit_and_predict("OneVsOne", modeloOneVsOne, treino_dados, treino_marcacoes)
+resultados[resultadoOneVsOne] = modeloOneVsOne
 
-scores = cross_val_score(modelo, treino_dados, treino_marcacoes, cv = k)
+from sklearn.naive_bayes import MultinomialNB
+modeloMultinomial = MultinomialNB()
+resultadoMultinomial = fit_and_predict("MultinomialNB", modeloMultinomial, treino_dados, treino_marcacoes)
+resultados[resultadoMultinomial] = modeloMultinomial
 
-print(scores)
+from sklearn.ensemble import AdaBoostClassifier
+modeloAdaBoost = AdaBoostClassifier()
+resultadoAdaBoost = fit_and_predict("AdaBoostClassifier", modeloAdaBoost, treino_dados, treino_marcacoes)
+resultados[resultadoAdaBoost] = modeloAdaBoost
 
-import numpy as np
-media = np.mean(scores)
+maximo = max(resultados)
+vencedor = resultados[maximo]
 
-print(media)
+print("Vencedor: ")
+print(vencedor)
+
+vencedor.fit(treino_dados, treino_marcacoes)
+
+def teste_real(modelo, validacao_dados, validacao_marcacoes):
+    resultado = modelo.predict(validacao_dados)
+    acertos = resultado == validacao_marcacoes
+
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(validacao_marcacoes)
+
+    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+    msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {0}".format(taxa_de_acerto)
+    
+    print(msg)
+
+teste_real(vencedor, validacao_dados, validacao_marcacoes)
+
+acerto_base = max(Counter(validacao_marcacoes).values())
+taxa_de_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
+
+total_de_elementos = len(validacao_dados)
+print("Total de teste: %d" % total_de_elementos)
